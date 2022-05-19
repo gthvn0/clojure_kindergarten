b43644d1bc8ed92c88bdaf6d83f40279d3c61b26
Guillaume Thouvenin <gthouvenin@kalray.eu>
2022-05-19 17:09:27 +0200

    movie: try to improve the algorithm

--- movie/src/movie/core.clj
+++ movie/src/movie/core.clj
@@ -99,20 +99,69 @@
   [card ticket perc x]
   (> (cost_systemA ticket x) (cost_systemB card ticket perc x)))
 
+
 (defn compute_movie
   [card ticket perc x]
   (if (card_is_profitable card ticket perc x)
     x
     (compute_movie card ticket perc (+ 1 x))))
 
-(defn movie
+(defn movie_basic
   "Return the number of times we need to go to cinema from which a card is
   profitable"
   [card ticket perc]
   (compute_movie card ticket perc 1))
 
 ; TEST
-(movie 500  15  0.9) ;; should return 43 
+(movie_basic 500  15  0.9) ;; should return 43
                      ;; (with card the total price is 634, with tickets 645)
-(movie 100  10  0.95) ;; should return 24 
+(movie_basic 100  10  0.95) ;; should return 24
                       ;; (with card the total price is 235, with tickets 240)
+
+;; movie_basic is O(n). We will try to reduce a little bit by looking by interval.
+;; For a given size we want to get the upper and lower bound. For example if the
+;; answer is 47 and the given size is ten we want to return (40 50)
+
+(defn interval_is_ok
+  "It takes two value and return true if card is profitable in this given interval"
+  [card ticket perc x y]
+  (and
+    (not (card_is_profitable card ticket perc x))
+    (card_is_profitable card ticket perc y)))
+
+(defn get_interval
+  "Return an interval of a given size where card is profitable"
+  [card ticket perc size base]
+  (loop [matcher [base (+ base size)]]
+    (if (interval_is_ok card ticket perc (first matcher) (second matcher))
+      matcher
+      (recur [(second matcher) (+ size (second matcher))]))))
+
+(interval_is_ok 500 15 0.9 10 200) ; must be true
+(interval_is_ok 500 15 0.9 10 20) ; must be false
+(interval_is_ok 500 15 0.9 100 200) ; must be false
+
+;; So we can start with an interval of 100
+(get_interval 500 15 0.9 100 0) ; should return [0 100]
+
+;; then interval of 10 using the lower value previously find
+(get_interval 500 15 0.9 10 0) ; should return [40 50]
+
+;; and now we can check all starting from 40
+(card_is_profitable 500 15 0.9 40)
+(card_is_profitable 500 15 0.9 41)
+(card_is_profitable 500 15 0.9 42)
+(card_is_profitable 500 15 0.9 43) ;; => got it
+
+(defn movie
+  "Return the number of times we need to go to cinema from which a card is
+  profitable"
+  [card ticket perc]
+  (let [first_try (get_interval card ticket perc 100 0)
+        second_try (get_interval card ticket perc 10 (first first_try))]
+    (loop [n (first second_try)]
+      (if (card_is_profitable card ticket perc n)
+        n
+        (recur (+ 1 n))))))
+
+(movie 500  15  0.9) ;; should return 43
