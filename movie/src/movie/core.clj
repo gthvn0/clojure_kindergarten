70ed6aeb7a23ba7e6d98dd2cf9095bf7243b1122
Guillaume Thouvenin <gthouvenin@kalray.eu>
2022-05-19 17:51:12 +0200

    movie: improve the compute of the cost of systemB

--- movie/src/movie/core.clj
+++ movie/src/movie/core.clj
@@ -6,49 +6,26 @@
   []
   (println "Hello, World!"))
 
-;; Cost of systemA is easy
+;; Cost of systemA
 (defn cost_systemA
   "Evaluate the cost of systemA"
   [ticket_price times]
   (* ticket_price times))
 
-;; Cost of systemB needs more compute. Let's decompose it
-;;
-;; If we go 3 times to cinema we need to apply the following
-;; discount must be applied to the ticket with 0.9 as percentage:
-;;   -> vec [ 0.9 0.9*0.9 0.9*0.9*0.9 ]
-;; So we need to construct this vec and we will need to create a
-;; function exponential:
-
-(defn exp
-  "return x**n"
-  [x n]
-  (reduce * (repeat n x)))
-
-;; Now we need to apply this to the price of the ticket. It is just
-;; a map of the partial function to the price of the ticket. The
-;; partial function is:
-(defn exp_x
-  [x]
-  (partial exp x))
-
-;; We will then add the price of the card. Note that the lenght of
-;; the vec is the number of times we will go to the cinema. Using range
-;; we can create a vec with the correct lenght. So the cost of B is
-;; computed as follow:
-
+;; Cost of systemB
 (defn cost_systemB
   "Evaluate the cost of systemB"
   [card_price ticket_price perc times]
-  (+ card_price
-     (reduce +
-             (map (partial * ticket_price)
-                  (map (exp_x perc)
-                       (vec (range 1 (+ times 1))))))))
+  (loop [p (* ticket_price perc)
+            idx 0
+            total card_price]
+    (if (= idx times)
+      total
+      (recur (* p perc) (+ idx 1) (+ total p)))))
 
 ;; now we can easily compare the price of the systemA and systemB if
-;; we now the number of times we are going to the cinema:
-(defn compare_A_B
+;; we know the number of times we are going to the cinema:
+(defn diff_costA_costB
   "Return the difference between the cost of A and the cost of B"
   [card_price ticket_price perc times]
   (- (cost_systemA ticket_price times)
@@ -65,7 +42,7 @@
   (fn [t]
     [(cost_systemA 15 t)
      (cost_systemB 500 15 0.9 t)
-     (compare_A_B 500 15 0.9 t)]))
+     (diff_costA_costB 500 15 0.9 t)]))
 
 ;; Let's start with 30
 (test1 30)
