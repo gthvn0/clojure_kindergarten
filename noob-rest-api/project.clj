d27935da25be5f56e1447e8ae0eb8f96c4108b17
Guillaume Thouvenin <gthouvenin@kalray.eu>
2022-05-19 13:26:13 +0200

    noob-rest-api: add dependencies and start the project

--- noob-rest-api/project.clj
+++ noob-rest-api/project.clj
@@ -3,7 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :dependencies [
+                 [org.clojure/clojure "1.10.3"]
+                 [compojure "1.6.3"] ;; routing lib
+                 [http-kit "2.5.3"]  ;; HTTP client server (event driven)
+                 [ring/ring-defaults "0.3.3"] ;; default configurations of Ring middleware
+                 [org.clojure/data.json "2.4.0"] ;; JSON parser/generator
+                 ]
   :main ^:skip-aot noob-rest-api.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- noob-rest-api/src/noob_rest_api/core.clj
+++ noob-rest-api/src/noob_rest_api/core.clj
@@ -1,7 +1,35 @@
 (ns noob-rest-api.core
+  (:require [org.httpkit.server :as server]
+            [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer :all]
+            [clojure.pprint :as pp])
   (:gen-class))
+
+(defn simple-body-page
+  [req]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body (str "Hello world<br>" req)})
+
+(defn request-example
+  [req]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body (->>
+           (pp/pprint req)
+           (str "Request Object: " req))})
+
+(defroutes app-routes
+  (GET "/" [] simple-body-page)
+  (GET "/request" [] request-example)
+  (route/not-found "Error, page not found!"))
 
 (defn -main
   "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (let [port (Integer/parseInt (or (System/getenv "PORT") "3000"))]
+    ;; Run the server with Ring.defaults middleware
+    (server/run-server (wrap-defaults #'app-routes site-defaults) {:port port})
+    (println (str "Running webserver at http://127.0.0.1:" port "/"))))
+
