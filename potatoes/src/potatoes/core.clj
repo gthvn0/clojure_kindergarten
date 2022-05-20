a56de58c5a554f8757a1bf258c5763e9aa2f98e3
Guillaume <gthouvenin@kalray.eu>
2022-05-20 18:38:16 +0200

    potatoes: new 7Kyu challenge

---
+++ potatoes/src/potatoes/core.clj
@@ -0,0 +1,7 @@
+(ns potatoes.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ potatoes/test/potatoes/core_test.clj
@@ -0,0 +1,7 @@
+(ns potatoes.core-test
+  (:require [clojure.test :refer :all]
+            [potatoes.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
