ed0ecdfef4629e59d9a1afbf5a290f0fcd2bca04
Guillaume <gthouvenin@kalray.eu>
2022-05-23 11:11:38 +0200

    codewars: new dir-reduc problem

---
+++ www.codewar.com/dir-reduc/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to dir-reduc
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ www.codewar.com/dir-reduc/test/dir_reduc/core_test.clj
@@ -0,0 +1,7 @@
+(ns dir-reduc.core-test
+  (:require [clojure.test :refer :all]
+            [dir-reduc.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
