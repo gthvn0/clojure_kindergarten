3920bf2cf9d435b1a445a46e08d1b7aac0cd02b5
Guillaume <gthouvenin@kalray.eu>
2022-05-23 10:12:16 +0200

    pentafibo: found a solution

--- pentafibonacci/src/pentafibonacci/core.clj
+++ pentafibonacci/src/pentafibonacci/core.clj
@@ -1,7 +1,18 @@
 (ns pentafibonacci.core
   (:gen-class))
 
+(defn pentafibo
+  ([] (pentafibo 0 1 1 2 4))
+  ([a b c d e] (lazy-seq (cons (bigint a) (pentafibo (bigint b) (bigint c) (bigint d) (bigint e) (+' a b c d e))))))
+
+; NOTE: After looking solutions it is possible to do it without bigint.
+;       A pattern exist and can be used...
+
+(defn count-odd-pentaFib
+  [n]
+  (- (count (filter odd? (vec (take (+ n 1) (pentafibo))))) 1))
+
 (defn -main
   "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println "Run lein test to check that function is working as expexted"))
--- pentafibonacci/test/pentafibonacci/core_test.clj
+++ pentafibonacci/test/pentafibonacci/core_test.clj
@@ -2,6 +2,15 @@
   (:require [clojure.test :refer :all]
             [pentafibonacci.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn test-assert [act exp]
+  (is (= act exp)))
+
+(deftest a-test1
+  (testing "count-odd-pentaFib : low values"
+    (let [lstI [45, 68, 76, 100, 121]
+          resultsI [15, 23, 25, 33, 40] ]
+      (loop [i 0]
+        (if (<= i 4)
+          (do
+            (test-assert (count-odd-pentaFib (lstI i)) (resultsI i))
+            (recur (inc i))))))))
