6d0766694c970757b7188f35fe7c934b4f6af633
Guillaume <gthouvenin@kalray.eu>
2022-05-23 11:14:49 +0200

    codewars: add test for dirReduc

--- www.codewar.com/dir-reduc/test/dir_reduc/core_test.clj
+++ www.codewar.com/dir-reduc/test/dir_reduc/core_test.clj
@@ -1,7 +1,9 @@
 (ns dir-reduc.core-test
-  (:require [clojure.test :refer :all]
-            [dir-reduc.core :refer :all]))
+  (:require [clojure.test :refer [deftest is testing]]
+            [dir-reduc.core :refer [dirReduc]]))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "Test 1"
+    (def ur ["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"])
+    (def vr ["WEST"])
+    (is (= (dirReduc ur) vr))))
